34f8b400f734ef03aec2aa333073d0af908c350f
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2011-02-06 10:22:37 +0100

    removed docstring from ->, which I had pasted in for reference.

--- src/main/clojure/bpsmithmannschott/adiutores/parva.clj
+++ src/main/clojure/bpsmithmannschott/adiutores/parva.clj
@@ -40,12 +40,6 @@ destructure."
   `(let [~bindable-form ~init-expression]
      ~@body))
 
-  "Threads the expr through the forms. Inserts x as the
-  second item in the first form, making a list of it if it is not a
-  list already. If there are more forms, inserts the first form as the
-  second item in second form, etc."
-
-
 (defmacro thread-with
   "Similar to the -> and ->> macros in clojure.core, but more flexible because
 one specifies the place where the previous expression is to be inserted
